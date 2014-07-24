a6fed3820570d72c533e55ae58dfc5fb2366422f
Hack Reactor Students <danthareja@gmail.com>
2014-07-24 11:31:29 -0700

    completed prototypal stack and prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,32 @@
 var makeQueue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = Object.create(queueMethods);
+
+  someInstance.results = {};
+  someInstance.enqLoc = 0;
+  someInstance.deqLoc = 0;
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue : function(item) {
+    this.results[this.enqLoc] = item;
+    this.enqLoc++;
+  },
+  dequeue : function() {
+    if ( this.enqLoc - this.deqLoc ) {
+      var result = this.results[this.deqLoc];
+      delete this.results[this.deqLoc];
+      this.deqLoc++;
+      return result;
+    }
+  },
+  size : function() {
+    return this.enqLoc - this.deqLoc;
+  }
+};
 
 
